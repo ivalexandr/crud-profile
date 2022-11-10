8d7e9f282e72beab1504aef6d8219fe81cd770ff
Alexandr Iv <ivanin.alexandr@outlook.com>
2022-11-10 09:50:48 +0300

    added origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = Number(process.env.PORT) || 3000;
-  const app = await NestFactory.create(AppModule, { cors: false });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: 'http://localhost:3000/' });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
