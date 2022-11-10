65270ca31a358e1ea867121ef7971d1d3ebaf97c
Alexandr Iv <ivanin.alexandr@outlook.com>
2022-11-11 01:36:59 +0300

    change cors origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PORT = Number(process.env.PORT) || 3000;
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:3000' });
+  app.enableCors({ origin: 'https://crud-front.onrender.com' });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
